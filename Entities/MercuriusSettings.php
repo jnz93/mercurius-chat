3ff95ba000703bf7bd3fe394004a40317ea54a33
joanezandrades <box@unitycode.tech>
2020-04-13 22:25:56 -0300

    Reformulando as opções de configurações do plugin

--- Entities/MercuriusSettings.php
+++ Entities/MercuriusSettings.php
@@ -18,20 +18,26 @@ class MercuriusSettings{
         register_setting($option_group, 'mchat_title');
         register_setting($option_group, 'mchat_description');
         
-        # Opções
-        register_setting($option_group, 'mchat_option_message');
-        register_setting($option_group, 'mchat_option_service_1');
-        register_setting($option_group, 'mchat_service_1_endpoint');
-        register_setting($option_group, 'mchat_option_service_2');
-        register_setting($option_group, 'mchat_service_2_endpoint');
-        register_setting($option_group, 'mchat_option_service_3');
-        register_setting($option_group, 'mchat_service_3_endpoint');
+        # MENU
+        register_setting($option_group, 'mchat_menu_title');
+        register_setting($option_group, 'mchat_menu_option_1');
+        register_setting($option_group, 'mchat_menu_endpoint_1');
+        register_setting($option_group, 'mchat_menu_option_2');
+        register_setting($option_group, 'mchat_menu_endpoint_2');
+        register_setting($option_group, 'mchat_menu_option_3');
+        register_setting($option_group, 'mchat_menu_endpoint_3');
+
+        # PAGES
+        register_setting($option_group, 'mchat_page_services_title');
+        register_setting($option_group, 'mchat_page_services_description');
+        register_setting($option_group, 'mchat_page_faq_title');
+        register_setting($option_group, 'mchat_page_faq_description');
 
         # Contact
-        register_setting($option_group, 'mchat_phone_number');
         register_setting($option_group, 'mchat_contact_message');
 
         # Another settings
+        register_setting($option_group, 'mchat_business_hours');
         register_setting($option_group, 'mchat_screen_position');
         register_setting($option_group, 'mchat_custom_icon');
         register_setting($option_group, 'mchat_tooltip');
