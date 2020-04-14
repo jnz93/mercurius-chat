8643f7ab07c0090ecd809a7865cb15a46c969080
joanezandrades <box@unitycode.tech>
2020-04-13 21:59:01 -0300

    Finalização dos templates html

---
+++ templates/front-end/chat.php
@@ -0,0 +1,58 @@
+<div class="mchatContainer mchatContainer--right">
+    <header class="mchatHeader">
+        <div class="mchatHeader__">
+            <span class=""><!-- Botão voltar --></span>
+        </div>
+        <h1 class="mchatHeader__title">Bem vindo</h1>
+        <p class="mchatHeader__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </header>
+
+    <section class="mchatBody">
+
+        <!-- Inicial -->
+        <div class="mchatBody__content">
+            <!-- Card Atendente -->
+            <div class="mchatCard">
+                <h3 class="mchatCard__title">Atendimento Online</h3>
+                <div class="mchatPerson">
+                    <div class="mchatPerson__thumbContainer mchatPerson__thumbContainer--48px">
+                        <img src="" alt="" class="mchatPerson__thumb mchatPerson__thumb--48px">
+                        <span class="mchatPerson__status mchatPerson__status--online"></span>
+                    </div>
+                    <div class="mchatPerson__infoContainer">
+                        <span class="mchatPerson__name">Janice M. Gleen</span>
+                        <span class="mchatPerson__role">Suporte</span>
+                    </div>
+                </div>
+                <button type="button" class="btnPrimary"><span class="btnPrimary__text">iniciar conversa</span><i class="icon__whatsapp icon--16px"></i></button>
+            </div>
+            <!-- /End -->
+
+            <h3 class="mchatBody__title">Menu rápido:</h3>
+            <ul class="mchatNav">
+                <li class="mchatNav__item"><a href="#" class="mchatNav__link"><i class="icon__arrowRight icon--16px"></i>Compra rápida</a></li>
+                <li class="mchatNav__item"><a href="#" class="mchatNav__link"><i class="icon__arrowRight icon--16px"></i>Perguntas Frequentes(FAQ)</a></li>
+            </ul>
+        </div>
+
+        <!-- Pages -->
+        <div class="mchatBody__containerResults" style="display: none">
+            <div id="page-products" class="">
+                <!-- Add produtos -->
+            </div>
+
+            <div id="page-details-product" class="" style="display: none;">
+                <!-- Add detalhes produto selecionado -->
+            </div>
+
+            <div id="page-faq" class="" style="display: none;">
+                <!-- FAQ page -->
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <footer class="mchatBody__footer">
+            <span class="mchatBody__label">Horário de atendimento: <b>09:00 AM - 18:00 PM</b></span>
+        </footer>
+    </section>
+</div>
